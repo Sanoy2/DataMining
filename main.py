3d9e1f8b2b518777aac0eb9fc6103512314bbca2
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2019-06-12 13:36:50 +0200

    Reading file

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
+filepath = "spam.dat"
+
 def main():
     print("Hello")
+    print(filepath)
+    file_content = pd.read_csv(filepath) # odzywam sie po naglowkach
+    print(file_content['ACT_NOW']) 
+
 
 if __name__ == "__main__":
     main()
